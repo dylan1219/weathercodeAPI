1138eb74f7f8262262bd5481d1fe73f77ca80659
dylan1219 <dylansanchez1219@gmail.com>
2024-12-16 21:34:33 +0800

    weathercode added

--- script.js
+++ script.js
@@ -24,6 +24,25 @@ const municipalities = {
     paluan: { lat: 13.3667, lon: 120.5333 },
     magsaysay: { lat: 12.316667, lon: 121.166667 },
 };
+//Weather Code Descriptions
+const weatherCodeDescriptions = {
+    0: "Di Mawaring Panahon",
+    1000: "Maliwanag ang Kalangitan(Simulan na ang Labada.) ",
+    1001: "Aba'y Medyo Maulap ang Kalangitan",
+    1100: "Ayy Mainit sa Umaga, Maglagay ng Sunscreen at Uminom ng maraming tubig",
+    1101: "Medyo Maulap ang Kalangitan pero hindi uulan",
+    1102: "Maulap,Di Makita ang Araw.Maulap din sa Gabi",
+    2000: "Mahamog parang Baguio",
+    2100: "Bahagyang Mahamog parang Tagaytay",
+    3000: "Mahangin ba Labas?",
+    3001: "Banayad na hangin.Masarap matulog",
+    3002: "Malakas ang Hangin Sa Labas",
+    4000: "Maambon, Ikaw ay magsuot ng Gora",
+    4001: "Magdala ka ng Payong. May Ulan na Bahagya",
+    4200: "Maulan ng Bahagya, Pero Magpayong ka pa rin",
+    4201: "Malakas ang ulan!.Iwasan magpagalagala",
+    8000: "Mag-ingat sa Kulog at Kidlat. Hindi ka si superman."
+};
 
 
 
